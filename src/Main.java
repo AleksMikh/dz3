399d32294cb1437036f26c1833c78373f3a3ff57
Mikhail Alekseev <rai1gun@mail.ru>
2023-03-09 22:49:57 +0300

    gitignore added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 25_400;
+        int miles = 20;
+        int BonusMiles = (price / miles);
+        System.out.println(BonusMiles);
+    }
+}
